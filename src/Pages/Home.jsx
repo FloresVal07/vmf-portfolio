133fd4f2be8fa80b1b47626a67ffe9007e1f7c18
FloresVal07 <floresvalentin300@gmail.com>
2025-01-06 16:39:44 -0700

    Homepage top half elements

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,21 @@
+import "./Home.css";
+
 function Home(){
     return(
-        <div className="content">
-            <h1>Home</h1>
+        <div className="content" id="home">
+            <div className="overview">
+                <h1 className="overview-title">Overview</h1>
+                <hr className="overview-line"/>
+                <p className="overview-text">Welcome to my personal website! To give you a basic run down, my name is Valentin Mateo Flores. 
+                    I’m a current Computer Science freshman at the Ira Fulton College at Arizona State University.
+                     I’m a proud Mexican and want to do everything in my power to learn and better the world. Be it
+                      through innovation, guidance, or anything I can do. This website will document all my 
+                      achievements and interests along the way.</p>
+                <hr className="overview-line"/>
+            </div>
+            <div className="overview-image">
+                <h1>Home</h1>
+            </div>
         </div>
     );
 }
