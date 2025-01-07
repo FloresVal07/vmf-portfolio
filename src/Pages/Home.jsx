7d1548d4604a9a87d54feeb92a1067358211a1e4
FloresVal07 <floresvalentin300@gmail.com>
2025-01-07 11:20:19 -0700

    Add responsiveness to the text

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,12 +5,15 @@ function Home(){
         <div className="content" id="home">
             <div className="overview">
                 <h1 className="overview-title">Overview</h1>
+                <br/>
                 <hr className="overview-line"/>
+                <br/>
                 <p className="overview-text">Welcome to my personal website! To give you a basic run down, my name is Valentin Mateo Flores. 
                     I’m a current Computer Science freshman at the Ira Fulton College at Arizona State University.
                      I’m a proud Mexican and want to do everything in my power to learn and better the world. Be it
                       through innovation, guidance, or anything I can do. This website will document all my 
                       achievements and interests along the way.</p>
+                <br/>
                 <hr className="overview-line"/>
             </div>
             <div className="overview-image">
