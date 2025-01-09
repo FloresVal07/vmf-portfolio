3e2625c7e4b38cd91adc85daeaf070a634842677
FloresVal07 <floresvalentin300@gmail.com>
2025-01-08 17:49:46 -0700

    Somewhat fixed carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,22 +5,24 @@ import Carousel from "./Carousel.jsx";
 function Home(){
     return(
         <div className="content" id="home">
-            <div className="overview">
-                <h1 className="overview-title">Overview</h1>
-                <br/>
-                <hr className="overview-line"/>
-                <br/>
-                <p className="overview-text">Welcome to my personal website! To give you a basic run down, my name is Valentin Mateo Flores. 
-                    I’m a current Computer Science freshman at the Ira Fulton College at Arizona State University.
-                     I’m a proud Mexican and want to do everything in my power to learn and better the world. Be it
-                      through innovation, guidance, or anything I can do. This website will document all my 
-                      achievements and interests along the way.</p>
-                <br/>
-                <hr className="overview-line"/>
-                <br/>
-            </div>
-            <div className="overview-image-holder">
-                <img src={selfie} className="overview-image" alt="selfie image of Valentin"/>
+            <div className="topHalf">
+                <div className="overview">
+                    <h1 className="overview-title">Overview</h1>
+                    <br/>
+                    <hr className="overview-line"/>
+                    <br/>
+                    <p className="overview-text">Welcome to my personal website! To give you a basic run down, my name is Valentin Mateo Flores. 
+                        I’m a current Computer Science freshman at the Ira Fulton College at Arizona State University.
+                        I’m a proud Mexican and want to do everything in my power to learn and better the world. Be it
+                        through innovation, guidance, or anything I can do. This website will document all my 
+                        achievements and interests along the way.</p>
+                    <br/>
+                    <hr className="overview-line"/>
+                    <br/>
+                </div>
+                <div className="overview-image-holder">
+                    <img src={selfie} className="overview-image" alt="selfie image of Valentin"/>
+                </div>
             </div>
             <Carousel/>
         </div>
